ac8ab5392b37df4ea19085ee9dee3c44f37f9203
knacko <geekbike@hotmail.com>
2026-06-10 13:00:29 -0600

    feat: starts unpowered

--- cilice.cpp
+++ cilice.cpp
@@ -18,41 +18,41 @@ const float maxAccel    = maxSpeed/2;
 const float startPos = 0    // This should correspond to the outside pin being center-right of the middle pin
 const float endPos   = -270   // This should correspond to the outside pin being center-bottom of the middle pin
 
-// Define motor interface type (1 for External Driver like A4988/DRV8825)
+// Define motor interface
 AccelStepper stepper(1, stepPin, dirPin);
 
 void setup() {
-  pinMode(forwPin, INPUT_PULLUP); // Button to GND, triggers LOW when pressed
+  pinMode(forwPin, INPUT_PULLUP);     // Triggers LOW when pressed
   pinMode(revPin,  INPUT_PULLUP);
   pinMode(stopPin, INPUT_PULLUP);
+  stepper.disableOutputs();           // Initial state is unpowered
   stepper.setMaxSpeed(maxSpeed);
   stepper.setAcceleration(maxAccel);
-  bool zeroSet = false
+  bool zeroSet = false                // Becomes true once the initial position is set via the stop button
 }
 
 void loop() {
-  if (digitalRead(revPin) == LOW)   
-    if (zeroSet) 
-      rotateDegrees(startPos)
+  if (digitalRead(revPin) == LOW) {
+    if (zeroSet) rotateDegrees(startPos); }
 
-  if (digitalRead(forwPin) == LOW) 
-    if (zeroSet)
-      rotateDegrees(endPos);
+  if (digitalRead(forwPin) == LOW) {
+    if (zeroSet) rotateDegrees(endPos); }
 
-  if (digitalRead(stopPin) == LOW) 
+  if (digitalRead(stopPin) == LOW) {
     stepper.stop();
-    while (stepper.distanceToGo() != 0)
-      stepper.run();
+    while (stepper.distanceToGo() != 0) {
+      stepper.run(); }
     delay(100);
     stepper.disableOutputs(); 
     stepper.setCurrentPosition(0);
     zeroSet = true;
+  }
 }
 
 void rotateDegrees(float degrees) {
   stepper.enableOutputs();
   long steps = round((degrees * gearRatio) / (360/stepsPerRev));
   stepper.moveTo(steps);
-  while (stepper.distanceToGo() != 0 && digitalRead(stopPin) != LOW) 
-    stepper.run();
+  while (stepper.distanceToGo() != 0 && digitalRead(stopPin) != LOW) {
+    stepper.run(); }
 }
